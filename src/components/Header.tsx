873eb06834372eb07e5e9f2c1e727f5349d40a7a
vlad <zakrevskiyvlada@yandex.ru>
2023-04-30 16:33:25 +0400

    pl

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ type Props = {}
 const Header = (props: Props) => {
   return (
     <Navbar bg='primary' variant='dark' className='header'>
-      <Navbar.Brand className='header_brand' style={{paddingLeft '10px'}}>
+      <Navbar.Brand className='header_brand' style={{paddingLeft: '10px'}}>
         Paint Online [Fun]
       </Navbar.Brand>
     </Navbar>
